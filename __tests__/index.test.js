0e0f23f6505d4488a7f2308469bb53c617010df1
Max <maxim.90svet@gmail.com>
2023-10-15 00:30:38 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,7 +6,7 @@ import getFileDiff from '../src/index.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const file1File2Diff = `{
+const ABDiff = `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -15,69 +15,67 @@ const file1File2Diff = `{
   + verbose: true
 }`;
 
-const file1FileEDiff = `{
+const AEDiff = `{
   - follow: false
   - host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
 }`;
 
-const file2FileEDiff = `{
+const EBDiff = `{
   + host: hexlet.io
   + timeout: 20
   + verbose: true
 }`;
 
-describe('Test getFileDiff for JSON', () => {
-  const file1RelativeAdress = '../__fixtures__/file1.json';
-  const file2RelativeAdress = '../__fixtures__/file2.json';
-  const epmtyJSONFileRelativeAdress = '../__fixtures__/fileE.json';
-  const file1AbsoluteAdress = resolve(__dirname, file1RelativeAdress);
-  const file2AbsoluteAdress = resolve(__dirname, file2RelativeAdress);
-  const wrongTypeAdress = '../__fixtures__/file1.html';
-  const wrongAdress = '../__fixtures__/file3.json';
+const EEDiff = `{
+
+}`;
+
+describe('Test getFileDiff for different address types', () => {
+  const relativePathA = '../__fixtures__/fileA.json';
+  const relativePathB = '../__fixtures__/fileB.json';
+  const absolutePathA = resolve(__dirname, relativePathA);
+  const absolutePathB = resolve(__dirname, relativePathB);
+  const wrongTypePath = '../__fixtures__/fileA.html';
+  const wrongPath = '../__fixtures__/fileC.json';
 
   test('absolute address tests', () => {
-    expect(getFileDiff(file1AbsoluteAdress, file2AbsoluteAdress)).toBe(file1File2Diff);
+    expect(getFileDiff(absolutePathA, absolutePathB)).toBe(ABDiff);
+  });
+
+  test('relative address tests', () => {
+    expect(getFileDiff(relativePathA, relativePathB)).toBe(ABDiff);
   });
 
   test('wrong file type tests', () => {
-    expect(() => getFileDiff(file1AbsoluteAdress, wrongTypeAdress)).toThrow('Unsupported file type -- PARSE');
+    expect(() => getFileDiff(absolutePathA, wrongTypePath))
+      .toThrow('Unsupported file type -- PARSE');
   });
 
   test('wrong address tests', () => {
-    expect(() => getFileDiff(file1AbsoluteAdress, wrongAdress)).toThrow();
-  });
-
-  test('relative address tests', () => {
-    expect(getFileDiff(file1RelativeAdress, file2RelativeAdress)).toBe(file1File2Diff);
-  });
-
-  test('tests with empty JSON', () => {
-    expect(getFileDiff(file1AbsoluteAdress, epmtyJSONFileRelativeAdress)).toBe(file1FileEDiff);
-    expect(getFileDiff(epmtyJSONFileRelativeAdress, file2AbsoluteAdress)).toBe(file2FileEDiff);
-    expect(getFileDiff(epmtyJSONFileRelativeAdress, epmtyJSONFileRelativeAdress)).toBe('{\n\n}');
+    expect(() => getFileDiff(absolutePathA, wrongPath)).toThrow();
   });
 });
 
-describe('Test getFileDiff for YAML', () => {
-  const file1RelativeAdress = '../__fixtures__/file1.yaml';
-  const file2RelativeAdress = '../__fixtures__/file2.yml';
-  const epmtyJSONFileRelativeAdress = '../__fixtures__/fileE.yaml';
-  const file1AbsoluteAdress = resolve(__dirname, file1RelativeAdress);
-  const file2AbsoluteAdress = resolve(__dirname, file2RelativeAdress);
+describe('Test getFileDiff for different file types', () => {
+  const pathToYamlA = '../__fixtures__/fileA.yaml';
+  const pathToJSONA = '../__fixtures__/fileA.json';
+  const pathToYmlB = '../__fixtures__/fileB.yml';
+  const epmtyYamlE = '../__fixtures__/fileE.yaml';
+  const epmtyJSONE = '../__fixtures__/fileE.json';
 
-  test('absolute address tests', () => {
-    expect(getFileDiff(file1AbsoluteAdress, file2AbsoluteAdress)).toBe(file1File2Diff);
+  test('test for YAML', () => {
+    expect(getFileDiff(pathToYamlA, pathToYmlB)).toBe(ABDiff);
   });
 
-  test('relative address tests', () => {
-    expect(getFileDiff(file1RelativeAdress, file2RelativeAdress)).toBe(file1File2Diff);
+  test('test for JSON and YAML', () => {
+    expect(getFileDiff(pathToJSONA, pathToYmlB)).toBe(ABDiff);
   });
 
-  test('tests with empty YAML', () => {
-    expect(getFileDiff(file1AbsoluteAdress, epmtyJSONFileRelativeAdress)).toBe(file1FileEDiff);
-    expect(getFileDiff(epmtyJSONFileRelativeAdress, file2AbsoluteAdress)).toBe(file2FileEDiff);
-    expect(getFileDiff(epmtyJSONFileRelativeAdress, epmtyJSONFileRelativeAdress)).toBe('{\n\n}');
+  test('tests with empty YAML and JSON', () => {
+    expect(getFileDiff(pathToYamlA, epmtyYamlE)).toBe(AEDiff);
+    expect(getFileDiff(epmtyJSONE, pathToYmlB)).toBe(EBDiff);
+    expect(getFileDiff(epmtyYamlE, epmtyJSONE)).toBe(EEDiff);
   });
 });
