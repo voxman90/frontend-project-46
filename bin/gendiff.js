d3c0010b04ee6006dc13699d2fb244397eb594b9
Max <maxim.90svet@gmail.com>
2023-10-14 16:07:08 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,9 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference')
-  .version('0.0.1');
+  .version('0.0.1')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 
 program.parse();
