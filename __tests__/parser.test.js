8ace45903db6963fe66b8a6b7aa773c9a1d43dff
Max <maxim.90svet@gmail.com>
2023-10-19 19:56:12 +0300

    code climate fixes

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -1,5 +1,8 @@
 import { expect, test } from '@jest/globals';
 
+import {
+  diffNested,
+} from '../__fixtures__/samples';
 import getDiff, {
   mapDiff,
   filterDiff,
@@ -66,47 +69,6 @@ const nestedObjB = {
     fee: 100500,
   },
 };
-
-const diffNested = [
-  {
-    weight: 3,
-    key: 'common',
-    value: [
-      { weight: 1, key: 'follow', value: false },
-      { weight: 0, key: 'setting1', value: 'Value 1' },
-      { weight: -1, key: 'setting2', value: 200 },
-      { weight: 2, key: 'setting3', value: { oldValue: true, newValue: null } },
-      { weight: 1, key: 'setting4', value: 'blah blah' },
-      { weight: 1, key: 'setting5', value: { key5: 'value5' } },
-      {
-        weight: 3,
-        key: 'setting6',
-        value: [
-          {
-            weight: 3,
-            key: 'doge',
-            value: [
-              { weight: 2, key: 'wow', value: { oldValue: '', newValue: 'so much' } },
-            ],
-          },
-          { weight: 0, key: 'key', value: 'value' },
-          { weight: 1, key: 'ops', value: 'vops' },
-        ],
-      },
-    ],
-  },
-  {
-    weight: 3,
-    key: 'group1',
-    value: [
-      { weight: 2, key: 'baz', value: { oldValue: 'bas', newValue: 'bars' } },
-      { weight: 0, key: 'foo', value: 'bar' },
-      { weight: 2, key: 'nest', value: { oldValue: { key: 'value' }, newValue: 'str' } },
-    ],
-  },
-  { weight: -1, key: 'group2', value: { abc: 12345, deep: { id: 45 } } },
-  { weight: 1, key: 'group3', value: { deep: { id: { number: 45 } }, fee: 100500 } },
-];
 
 test('Test getDataDiff for nested objects', () => {
   expect(getDiff(nestedObjA, nestedObjB)).toEqual(diffNested);
