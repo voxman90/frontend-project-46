32180825bb78e0c4075b9622be55bba7cef1e4cd
Max <maxim.90svet@gmail.com>
2023-10-25 03:52:20 +0300

    minor tests fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-import { resolve, dirname, join } from 'path';
+import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { expect, describe, test } from '@jest/globals';
 
@@ -10,15 +10,16 @@ import {
 } from '../__fixtures__/samples.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const fileDir = resolve(__dirname, '../__fixtures__/');
+const fixtureDir = join(__dirname, '..', '__fixtures__');
+const getFixturePath = (fileName, fileExt) => join(fixtureDir, `${fileName}.${fileExt}`);
 
 describe.each([
   ['json'],
   ['yaml'],
   ['yml'],
 ])('Test getFileDiff for \'%s\' type file', (fileExt) => {
-  const fileAPath = join(fileDir, `nestedA.${fileExt}`);
-  const fileBPath = join(fileDir, `nestedB.${fileExt}`);
+  const fileAPath = getFixturePath('nestedA', fileExt);
+  const fileBPath = getFixturePath('nestedB', fileExt);
 
   test.each([
     ['stylish', stylishNestedDiff],
